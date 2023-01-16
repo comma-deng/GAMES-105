160e289d904956b8a1f4abdbfeeee3e391b142a9
comma <comma@zju.edu.cn>
2023-01-16 23:34:25 +0800

    smooth transform WIP

--- lab2/answer_task2.py
+++ lab2/answer_task2.py
@@ -9,6 +9,7 @@ position_weight = 1
 vel_weight = 1
 joint_position_weight = 10
 joint_vel_weight = 10
+half_life = 0.2
 bvh_folder_path = r'motion_material\kinematic_motion'
 
 def find_min_cost(motion_key_desire, motion_key_real):
@@ -22,6 +23,15 @@ def find_min_cost(motion_key_desire, motion_key_real):
 
     return np.argmin(res), res[np.argmin(res)]
 
+class DampingParams:
+    def __init__(self) -> None:
+        self.pos_diff = None
+        self.vel_diff = None
+        self.rot_diff = None
+        self.avel_diff = None
+        self.time = 0
+        self.need_damping = False
+
 class CharacterController():
     def __init__(self, controller) -> None:
         self.motions = []
@@ -31,6 +41,7 @@ class CharacterController():
         self.cur_root_rot = None
         self.cur_frame = 0
         self.cur_seq = 0
+        self.dampingParams = DampingParams()
         self.motion_keys = []
         for file in os.listdir(bvh_folder_path):
             if not file.endswith('keys'):
@@ -42,7 +53,7 @@ class CharacterController():
             self.motions.append(BVHMotion(bvh_file_name))
             f.close()
         pass
-    
+
     def update_state(self, 
                      desired_pos_list, 
                      desired_rot_list,
@@ -76,16 +87,38 @@ class CharacterController():
         # joint_orientation = joint_orientation[self.cur_frame]
 
         #TDOD 自己的代码
+        # print("joint_position_outside", self.motions[0].joint_position[0])
+        # print("joint_position_30", self.motions[0].joint_position[30])
+        frame_position = self.motions[self.cur_seq].joint_position[[self.cur_frame, self.cur_frame+1]].copy()
+        frame_position[:, 0, [0,2]] = desired_pos_list[0, [0,2]]
+        frame_rotation = self.motions[self.cur_seq].joint_rotation[[self.cur_frame, self.cur_frame+1]].copy()
+        #apply damping
+        if self.dampingParams.need_damping:
+            # print("self.dampingParams.pos_diff", self.dampingParams.pos_diff)
+            # print("self.dampingParams.vel_diff", self.dampingParams.vel_diff)
+            # print("self.dampingParams.rot_diff", self.dampingParams.rot_diff)
+            # print("self.dampingParams.avel_diff", self.dampingParams.avel_diff)
+            offset, vel = decay_spring_implicit_damping_pos(
+                self.dampingParams.pos_diff, self.dampingParams.vel_diff, half_life, self.dampingParams.time
+            )
+            offset_rot, avel = decay_spring_implicit_damping_rot(
+                self.dampingParams.rot_diff, self.dampingParams.avel_diff, half_life, self.dampingParams.time
+            )
+            frame_position += offset
+            # print(offset)
+            for i in range(2):
+                frame_rotation[i] = (R.from_rotvec(offset_rot) * R.from_quat(frame_rotation[i])).as_quat() 
+            self.dampingParams.time += 1/60
         real_key = MotionKey(1)
         real_key.positions[0] = desired_pos_list
         for i in range(6):
             real_key.positions[0][5-i] -= desired_pos_list[0]
         real_key.velocities[0] = desired_vel_list
         real_key.tracking_joints = self.motion_keys[0].tracking_joints
+        joint_translation, joint_orientation = self.motions[self.cur_seq].batch_forward_kinematics(frame_position, frame_rotation)
         for k in range(len(real_key.tracking_joints)):
             joint = real_key.tracking_joints[k]
             idx = self.motions[self.cur_seq].joint_name.index(joint)
-            joint_translation, joint_orientation = self.motions[self.cur_seq].forward_kinematics_at_index([self.cur_frame, self.cur_frame+1])
             real_key.joint_postions[0][k] = joint_translation[0, idx, :] - joint_translation[0, 0, :]
             real_key.joint_velocities[0][k] = (joint_translation[1, idx, :] - joint_translation[0, idx, :]) * 60
 
@@ -103,20 +136,36 @@ class CharacterController():
         if min_seq_id == self.cur_seq and abs(min_frame_id-self.cur_frame) < 10:
             self.cur_frame = (self.cur_frame + 1) % self.motions[0].motion_length
         else:
+            #calc dampParams
+            self.dampingParams.time = 0
+            joint_position_dst = self.motions[min_seq_id].joint_position[[min_frame_id, min_frame_id+1]].copy()
+            joint_rotation_dst = self.motions[min_seq_id].joint_rotation[[min_frame_id, min_frame_id+1]].copy()
+            self.dampingParams.pos_diff = (frame_position[0] - joint_position_dst[0])
+            self.dampingParams.pos_diff[0] = np.array([0,0,0])
+            vel_src = (frame_position[1] - frame_position[0]) * 60
+            vel_dst = (joint_position_dst[1] - joint_position_dst[0]) * 60
+            self.dampingParams.vel_diff = vel_src - vel_dst
+
+            avel_dst = quat_to_avel(joint_rotation_dst, 1/60)
+            avel_src = quat_to_avel(frame_rotation, 1/60)
+            self.dampingParams.rot_diff = (R.from_quat(frame_rotation[0]) * R.from_quat(joint_rotation_dst[0].copy()).inv()).as_rotvec()
+            self.dampingParams.avel_diff = avel_src[0] - avel_dst[0]
+            # print("rot_diff ", rot_diff)
+            # print("self.dampingParams.avel_diff ", self.dampingParams.avel_diff)
+            # print("half_life ", half_life)
+            #set new seq and frame
             self.cur_seq = min_seq_id
             self.cur_frame = min_frame_id
+            self.dampingParams.need_damping = True
+
         joint_name = self.motions[self.cur_seq].joint_name
-        frame_position = self.motions[self.cur_seq].joint_position[self.cur_frame]
-        frame_position[0, [0,2]] = desired_pos_list[0, [0,2]]
-        frame_position = frame_position.reshape(1, frame_position.shape[0], frame_position.shape[1])
-        frame_rotation = self.motions[self.cur_seq].joint_rotation[self.cur_frame]
-        frame_rotation = frame_rotation.reshape(1, frame_rotation.shape[0], frame_rotation.shape[1])
-        joint_translation, joint_orientation = self.motions[self.cur_seq].batch_forward_kinematics(frame_position, frame_rotation)
-        joint_translation = joint_translation[0]
+
+        translation_offset = desired_pos_list[0] - joint_translation[0][0]
+        translation_offset[1] = 0
+        joint_translation = joint_translation[0] + translation_offset
         joint_orientation = joint_orientation[0]
         self.cur_root_pos = joint_translation[0]
         self.cur_root_rot = joint_orientation[0]
-        # self.cur_frame = (self.cur_frame + 1) % self.motions[0].motion_length
         # print(self.cur_frame, self.cur_seq)
         return joint_name, joint_translation, joint_orientation
     
